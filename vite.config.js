58a5ac2ba37c76128c62c51cfb853005ef686f74
Randel John <randeljohngonzales6@gmail.com>
2026-02-28 11:49:06 +0800

    fix

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,8 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  // ADD THIS LINE BELOW (Use your exact GitHub repo name)
+  base: '/mental-health-ui/', 
   plugins: [
     vue(),
     vueDevTools(),
