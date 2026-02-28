24f3e7eaf8ffd4e362176ebb25b72da3e7247aea
Randel John <randeljohngonzales6@gmail.com>
2026-02-28 12:07:06 +0800

    api fix

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
   // Pointing to your Node server's API prefix
-  baseURL: 'http://localhost:3000/api' 
+  baseURL: 'https://mental-health-ui-drax.onrender.com' 
 });
 
 export default api;
